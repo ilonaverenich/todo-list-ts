f7649b2ca358ac1f31406350db0e02fabbb5e6bd
Ilona <Ilonaverenich@tut.by>
2024-01-13 16:53:57 +0300

    added done task

--- src/TODO/List.tsx
+++ src/TODO/List.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Button } from 'antd';
+import React from 'react';
+import { Todo } from './TodoList';
 
-const List = () => {
+interface ListProps {
+  list: Todo;
+  index:number,
+  deleteItem: (item:number)=>void;
+  setDone: (id: number) => void;
+}
+
+
+const List:React.FC<ListProps> = ({list,index,deleteItem,setDone}) => {
+  console.log(list)
   return (
-    <div>List</div>
+    <div className='container-item' onClick={() => setDone(list.id)}>
+      <b>{index+1}.</b><span className={list.done?'item item-active':'item'}>{list.value}</span> <Button className='btn' onClick={()=>deleteItem(list.id)}>X</Button>
+    
+    </div>
   )
 }
 
--- src/TODO/TodoList.tsx
+++ src/TODO/TodoList.tsx
@@ -1,26 +1,41 @@
 import React,{useState, ChangeEvent} from 'react';
-import {Input, Button} from 'antd'
+import {Input, Button} from 'antd';
+import List from './List'
 
-interface Todo {
+export interface Todo {
     id:number,
     value:string,
-    isEdit:boolean
+    isEdit:boolean,
+    done: boolean
 }
 
 const TodoList:React.FC = () => {
     const [todo, setTodo] = useState<Todo[]>([])
     const [value, setValue] = useState<string>('')
     const [isEdit, setIsEdit] = useState<boolean>(false)
+    const [done, setTodoDone] = useState<boolean>(false)
     const  handleChange = (event:ChangeEvent<HTMLInputElement>) => {
         setValue(event.target.value)
     }
 
     const addTask = () => {
         console.log(todo)
-        setTodo([...todo, { id: todo.length + 1, value: value, isEdit: false }]) 
+        setTodo([...todo, { id: todo.length + 1, value: value, isEdit: false, done:false }]) 
         setValue('')
     }
- 
+    const deleteItem = (id:number) =>{
+        console.log(id)
+        setTodo(todo.filter(el=>el.id!==id))
+    }
+    const setDone = (id: number) => {
+        setTodo((prevTodo) =>
+          todo.map((item) =>
+            item.id === id ? { ...item, done: !item.done } : item
+          )
+        );
+      };
+
+
   return (
     <div>
         <h1>TODO LIST</h1>
@@ -31,7 +46,7 @@ const TodoList:React.FC = () => {
         </div>
 
         <div className='content-list'>
-            {todo.map((list, index)=> <p>{index+1}.{list.value}</p> )}
+            {todo.map((list, index)=><List list={list} index={index} deleteItem={deleteItem} setDone={setDone}/>)}
         </div>
 
     </div>
